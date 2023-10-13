a5c60b8bac3f9355df5d5c9169cdbb6af8338db5
Brundes <pedro.pac97@gmail.com>
2023-10-12 21:23:32 -0300

    Feat - application.properties - Created the console for the table Feat - IUserRepository - Created the Interface Feat - pom.xml - inserted new dependencies Feat - UserController - Implementation created for the users in addition of the RestDog Feat - UserModel - DTO created (information of the users)

--- src/main/java/br/com/pedrobrundes/todolist/user/UserController.java
+++ src/main/java/br/com/pedrobrundes/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.pedrobrundes.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integer (int) numeros inteiros
-     * Double (double) numeros 0.0000
-     * Float (float) numeros 0.000
-     * char (A C )
-     * Date (data)
-     * void
-     */
-    /**
-     * Body
-     * @param userModel
-     */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
